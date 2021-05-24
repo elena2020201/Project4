d454626cf5a26af4fd10312e0b1ccc72d1157dd8
elena2020201 <elenarus1992@mail.ru>
2021-05-24 18:57:55 +0300

    Добавьте файлы проекта.

---
+++ сумма квадратов нат чисел ду вайл/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace сумма_квадратов_нат_чисел_ду_вайл
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 10, k = 1, s = 0;
+            Console.Write("Сумма 1^2 + 2^2 + 3^2 + ... + {0} =", ((n * (n + 1)) * ((2 * n + 1))) / 6);
+            do
+            {
+                s += ((k * (k + 1)) * ((2 * k + 1))) / 6;
+                k++;
+            }
+            while
+                (k<=n);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
